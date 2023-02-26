b8467f8b744bdfc72828dbda44b826c8b0c6e8fc
Benoit Bertrand <benoit.bertrand@live.fr>
2023-02-26 23:33:09 +0100

    Init backend

---
+++ dc-backend/src/test/java/com/example/dcbackend/DcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
